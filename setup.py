52b532360cc528f194527d0b9473ae9213b842fb
georges <georges@opendatakosovo.org>
2015-09-21 11:44:00 +0200

    Minor update on setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ setup(
   name='cyrtranslit',
   packages=['cyrtranslit'],
   version='0.2',
-  description='Bi-directional Cyrillic transliterator for Python. Transliterate Cyrillic text to Latin and vice versa.',
+  description='Bi-directional Cyrillic transliteration. Transliterate Cyrillic text to Latin and vice versa.',
   author='Open Data Kosovo',
   author_email='dev@opendatakosovo.org',
   url='https://github.com/opendatakosovo/cyrillic-transliteration',
   download_url='https://github.com/opendatakosovo/cyrillic-transliteration/archive/v0.2.tar.gz',
-  keywords=['cyrillic', 'transliteration', 'transliterate', 'cyrtranslit', 'serbian', 'macedonian', 'montenegrin'],
+  keywords=['cyrillic', 'latin' 'transliteration', 'transliterate', 'cyrtranslit', 'serbian', 'macedonian', 'montenegrin'],
   classifiers=[],
 )
