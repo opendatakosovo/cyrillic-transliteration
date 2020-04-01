ebdce80e4c3643e6e3f209fb1cb74fa91f1ba512
Georges Labrèche <georges@opendatakosovo.org>
2020-04-01 17:59:22 -0400

    #12: allow input via pipe

--- cyrtranslit.py
+++ cyrtranslit.py
@@ -1,7 +1,8 @@
 import cyrtranslit
 from cyrtranslit.mapping import TRANSLIT_DICT
 from argparse import ArgumentParser
-import os.path
+import os
+import sys
 
 
 def __is_valid_input_file(parser, arg):
@@ -12,6 +13,9 @@ def __is_valid_input_file(parser, arg):
 
     if not os.path.exists(arg):
         parser.error("The file %s does not exist." % arg)
+    elif not os.isatty(0):
+        # Cannot process two inputs at once (i.e. from stdin and from file).
+        parser.error("Cannot process two inputs at once. Input data either from file or stdin but not from both.")
     else:
         return open(arg, 'r')
 
@@ -32,8 +36,8 @@ def __open_output_file(arg):
 parser = ArgumentParser(description="Transiliterate text in a given file.")
 
 # Input file.
-# Required.
-parser.add_argument("-i", dest="input_file", required=True,
+# Not required.
+parser.add_argument("-i", dest="input_file", required=False,
                     help="input file",
                     type=lambda x: __is_valid_input_file(parser, x))
 
@@ -49,14 +53,25 @@ parser.add_argument("-l", dest="language_code", required=True,
                     help="two-letter ISO 639-1 language code of cyrillic text",
                     type=lambda x: __is_valid_language_code(parser, x))
 
+# Flag for reverse transliteration, i.e. from latin/roman alphabet to cyrillic.
 parser.add_argument("-c", dest="to_cyrillic", action='store_true',
                     help="Parse latin characters to cyrillic (reverse of transliteration)")
 
 # Parse arguments
 args = parser.parse_args()
 
+# Get text input from stdin or from file.
+if not os.isatty(0):
+    # Read stdin.
+    text_input = sys.stdin.read()
+else:
+    # Read file.
+    text_input = args.input_file.read()
+
+    # Close input stream.
+    args.input_file.close()
+
 # Fetch arguments.
-text_input = args.input_file.read()
 lang_code = args.language_code
 file_output = args.output_file
 to_cyrillic = args.to_cyrillic
@@ -69,11 +84,11 @@ else:
 
 # Write output.
 if file_output is None:
+    # Print output if not output file is specified.
     print(text_output)
+
 else:
     file_output.write(text_output)
+
     # Close output stream.
     file_output.close()
-
-# Close input stream.
-args.input_file.close()
